c94315b77361252843f85db7f1c867e02f38810a
Arnav Mandlik <arnavmandlik1@gmail.com>
2024-04-08 07:36:19 +0530

    alpha v01

---
+++ trainer/trainer.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Create a new YOLO model from scratch
+model = YOLO('yolov8n.yaml')
+
+# Train the model using the 'coco128.yaml' dataset for 3 epochs
+results = model.train(data='config.yaml', epochs=1)
